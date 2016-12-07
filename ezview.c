ff25a5392c5ea290c61d2573faa2ca9bf782fbbc
jbredeme <jpb64@nau.edu>
2016-12-07 09:02:44 -0700

    Add in ppm reader functionality

--- ezview.c
+++ ezview.c
@@ -6,49 +6,267 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-#include "linmath.h"
+#include <math.h>
+#include <ctype.h>
+#include "ppm\ppm.h"
+#include "linmath\linmath.h"
 
-typedef struct {
-  float Position[2];
-  float TexCoord[2];
+
+/**
+ * This function checks if three integer values against a maximum and minimum values and its primary function 
+ * to check color channels.
+ *
+ * @param red - integer value represents the red color channel
+ * @param green - integer value represents the green color channel
+ * @param blue - integer value represents the blue color channel
+ * @param max - integer value that represents the color channels maximum value
+ * @returns min - integer value that represents the color channels maximum value
+ */
+int check_rgb_bits(int red, int green, int blue, int max, int min) {
+	if(((red > max) || (red < min)) || ((green > max) || (green < min)) || ((blue > max) || (blue < min))){
+		return(1);
+		
+	} else {
+		return(0);
+		
+	}
+		
+}
+
+
+/**
+ * Takes in two pointers as parameters, a filename of a ppm image and image structure
+ * use to store data read in from the ppm image file.
+ *
+ * @param filename - string pointer that represents a file name
+ * @param image - an image structure 
+ */
+void read_image(char *filename, Image *image) {
+	
+    char buffer[64];
+	FILE *fpointer;
+	int row, column, red, green, blue;
+	
+	// Open file steam for reading
+	fpointer = fopen(filename, "r");
+	
+	// Check to see if file was opened successfully
+	if(fpointer == NULL) {
+		fprintf(stderr, "Error, unable to open file.\n");
+
+		// Close file stream flush all buffers
+		fclose(fpointer);
+		exit(-1);
+		 
+	} else {
+		// Make sure we are reading from the beginning of the file
+		rewind(fpointer);
+
+		// Read in the first to characters
+		buffer[0] = fgetc(fpointer);
+		buffer[1] = fgetc(fpointer);
+
+		// Check the magic number
+		if((buffer[0] == 'P') && (buffer[1] == '6')) {
+			image->magic_number = "P6";
+
+		} else if((buffer[0] == 'P') && (buffer[1] == '3')) {
+			image->magic_number = "P3";
+
+		} else {
+			 fprintf(stderr, "Error, unacceptable image format while reading in the file.\n Magic number must be P6 or P3.\n");
+			 exit(-2);
+			 
+		}
+
+		// Ignore comments, whitespaces, carrage returns, and tabs
+		while(isdigit(buffer[0]) == 0){
+			// If you run into a comment proceed till you reach an newline character
+			if(buffer[0] == '#') {
+				do {
+					buffer[0] = fgetc(fpointer);
+					
+				} while(buffer[0] != '\n');
+
+			} else {
+				buffer[0] = fgetc(fpointer);
+				
+			}
+
+		}
+		
+		// Move back one character, tried using 
+		ungetc(buffer[0], fpointer);
+
+		// Read in <width> whitespace <height>
+		if(fscanf(fpointer, "%d %d", &image->width, &image->height) != 2) {
+			 fprintf(stderr, "Error, invalid width and/or height while reading in the file.\n");
+			 exit(-2);
+			 
+		}
+		
+		// Read in <maximum color value>
+		if(fscanf(fpointer, "%d", &image->max_color) != 1) {
+			 fprintf(stderr, "Error, invalid maximum color value.\n");
+			 exit(-2);
+			 
+		}
+
+		// Validate 8-bit color value
+		if((image->max_color > 255) || (image->max_color < 0)) {
+			 fprintf(stderr, "Error, input file's maximum color value is not 8-bits per channel.\n");
+			 exit(-2);
+			 
+		}
+		
+		// Allocated memory size for image data
+		image->image_data = malloc(sizeof(Pixel) * image->width * image->height);
+
+		// If magic number is P6 fread, if magic number is P3 for loop
+		if(image->magic_number[1] == '6') {
+			// Advance the file pointer by one char
+			fgetc(fpointer);
+			
+			// Read in raw image data
+			fread(image->image_data, sizeof(Pixel), image->width * image->height, fpointer);
+						
+		} else if(image->magic_number[1] == '3') {
+			// Read in ascii image data
+			for(row = 0; row < image->height; row++) {
+				for(column = 0; column < image->width; column++) {					
+
+					// Have to store values into int, larger values more then 1 byte will be truncated
+					// therefore making it not possible to check for color channels values over 8-bits	
+					fscanf(fpointer, "%d", &red);
+					fscanf(fpointer, "%d", &green);
+					fscanf(fpointer, "%d", &blue);					
+					
+					if(check_rgb_bits(red, green, blue, 255, 0) == 1) {
+						fprintf(stderr, "Error, a channel color value is not 8-bits.\n");
+						exit(-3);
+						
+					} else {
+						image->image_data[(image->width) * row + column].red = red;
+						image->image_data[(image->width) * row + column].green = green;
+						image->image_data[(image->width) * row + column].blue = blue;
+						
+					}					
+		
+				}
+				
+			}
+			
+		} else {
+			fprintf(stderr, "Error, invalid magic number.\n");
+			exit(-2);
+			
+		}
+
+		// Close file stream flush all buffers
+		fclose(fpointer);	
+		
+	}
+	
+}
+
+
+const GLubyte Indices[] = {
+	0, 1, 2,
+	2, 3, 0
+  
+};
+
+
+typedef struct Vertex {
+	float Position[2];
+	float TexCoord[2];
+  
 } Vertex;
 
+
 Vertex vertexes[] = {
-  {{1, -1}, {0.99999, 0}},
-  {{1, 1},  {0.99999, 0.99999}},
-  {{-1, 1}, {0, 0.99999}}
+	{{-1, 1}, {0, 0}},
+	{{1, 1}, {1, 0}},
+
+	{{-1, -1}, {0, 1}},
+	{{1, 1}, {1, 0}},
+
+	{{1, -1}, {1, 1}},
+	{{-1, -1}, {0, 1}}
+  
 };
 
 static const char* vertex_shader_text =
-"uniform mat4 MVP;\n"
-"attribute vec2 TexCoordIn;\n"
-"attribute vec2 vPos;\n"
-"varying vec2 TexCoordOut;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    TexCoordOut = TexCoordIn;\n"
-"}\n";
+	"uniform mat4 MVP;\n"
+	"attribute vec2 TexCoordIn;\n"
+	"attribute vec2 vPos;\n"
+	"varying vec2 TexCoordOut;\n"
+	"void main()\n"
+	"{\n"
+	"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+	"    TexCoordOut = TexCoordIn;\n"
+	"}\n";
 
 static const char* fragment_shader_text =
-"varying lowp vec2 TexCoordOut;\n"
-"uniform sampler2D Texture;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = texture2D(Texture, TexCoordOut);\n"
-"}\n";
-
-static void error_callback(int error, const char* description)
-{
+	"varying lowp vec2 TexCoordOut;\n"
+	"uniform sampler2D Texture;\n"
+	"void main()\n"
+	"{\n"
+	"    gl_FragColor = texture2D(Texture, TexCoordOut);\n"
+	"}\n";
+	
+	
+/**
+ * error_callback
+ *
+ * @param
+ * @param 
+ * @param
+ */
+static void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
+	
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+/**
+ * key_callback
+ *
+ * @param
+ * @param 
+ * @param
+ */
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {			//<= Close Window
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+		
+	} else if(key == GLFW_KEY_KP_8 && action == GLFW_PRESS) {     	//<= Scale(Up)
+		
+	} else if(key == GLFW_KEY_KP_2 && action == GLFW_PRESS) {     	//<= Scale(Down)
+		
+	} else if(key == GLFW_KEY_KP_4 && action == GLFW_PRESS) {     	//<= Rotate(CCW)
+		
+	} else if(key == GLFW_KEY_KP_6 && action == GLFW_PRESS) { 		//<= Rotate(CW)
+		
+	} else if(key == GLFW_KEY_KP_7 && action == GLFW_PRESS) { 		//<= Translation(Left)
+		
+	} else if(key == GLFW_KEY_KP_9 && action == GLFW_PRESS) { 		//<= Translation(Right)
+		
+	} else if(key == GLFW_KEY_KP_1 && action == GLFW_PRESS) { 		//<= Sheer(Left)
+		
+	} else if(key == GLFW_KEY_KP_3 && action == GLFW_PRESS) { 		//<= Sheer(Right)
+		
+	}
+        
 }
 
+/**
+ * glCompileShaderOrDie
+ *
+ * @param
+ * @param 
+ * @param
+ */
 void glCompileShaderOrDie(GLuint shader) {
   GLint compiled;
   glCompileShader(shader);
@@ -68,7 +286,7 @@ void glCompileShaderOrDie(GLuint shader) {
   }
 }
 
-// 4 x 4 image..
+
 unsigned char image[] = {
   255, 0, 0, 255,
   255, 0, 0, 255,
@@ -91,8 +309,10 @@ unsigned char image[] = {
   255, 0, 255, 255
 };
 
-int main(void)
-{
+int main(int argc, char *argv[]) {
+	FILE *fpointer;
+	Image *ppm_image;
+	
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
     GLint mvp_location, vpos_location, vcol_location;
